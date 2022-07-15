aef108fe39f84cd2f686ec781750d8b8fd8520b8
Oliver Veits @ gmail <oliver.veits@gmail.com>
2022-07-15 14:44:45 +0000

    master installed and configured playwright and added a first test with headless false to see the browser

---
+++ playwright.config.ts
@@ -0,0 +1,15 @@
+// playwright.config.ts
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './e2e-playwright',
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:4200/'
+  // },
+  use: {
+    //   browserName: 'chromium',
+    headless: false
+  },
+};
+export default config;
